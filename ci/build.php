b72d1bee822516c59a6a2af62923654e20b9d72c
Franck DIOMANDE <fkdiomande@gmail.com>
2019-05-28 21:50:20 +0200

    ci(common): deploy docs and data

--- ci/build.php
+++ ci/build.php
@@ -6,36 +6,36 @@ use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 
 $fileSystem = new Filesystem();
-
 $result = [];
-
+$docResult = [];
 $composeFinder = new Finder();
-
 $composeFinder->directories()->depth(0)->in('/app/src')->sortByName();
 
 foreach ($composeFinder as $compose) {
-
     $name = $compose->getBasename();
-
     $files = [
         'name' => $name,
         'files' => []
     ];
-
     $filesFinder = new Finder();
     $filesFinder->files()->in('/app/src/' . $name);
 
     foreach ($filesFinder as $file) {
-
+        $filename = $file->getFilename();
         $files['files'][] = [
             'directory' => $file->getRelativePath(),
-            'filename' => $file->getFilename(),
+            'filename' => $filename,
             'contents' => $file->getContents(),
         ];
-
+        if($filename === ($name . '.config.json')){
+            $doc = json_decode($file->getContents(), true);
+            $docResult[] = $doc;
+        }
     }
 
     $result[] = $files;
 }
 
-$fileSystem->appendToFile('/app/packages.json', json_encode($result));
+$fileSystem->remove(['/app/data/packages.json', '/app/doc/packages.json']);
+$fileSystem->dumpFile('/app/data/packages.json', json_encode($result));
+$fileSystem->dumpFile('/app/doc/packages.json', json_encode($docResult));
